95cedf27888923e9f0dd9f0f95e60bdb18dde756
Bogdan Simon <simonbogdandorin@gmail.com>
2025-07-11 15:19:57 +0200

    fix formating issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white text-black">
-        <h1 className="text-3xl text-center font-serif text-gray-900">
+      <h1 className="text-3xl text-center font-serif text-gray-900">
         Bine ai venit în Re-Verb
       </h1>
     </main>
