ab9abeb9ad8d669048865d8defafd1fed644a9d8
Bogdan Simon <simonbogdandorin@gmail.com>
2025-07-11 15:12:23 +0200

    chore: project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-white text-black">
+        <h1 className="text-3xl text-center font-serif text-gray-900">
+        Bine ai venit în Re-Verb
+      </h1>
+    </main>
+  );
+}
